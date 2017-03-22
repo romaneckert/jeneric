0c36ccccf1ae79cd6ce9942883eeb88c12c39eb0
Roman Eckert <mail@romaneckert.de>
2017-03-22 17:07:17 +0100

    work on config module

--- core/config.js
+++ core/config.js
@@ -5,17 +5,23 @@ class Config {
 
     constructor() {
 
-        this.path = {
-            root : path.dirname(require.main.filename)
+        this.config = {
+            file : 'config/config.json'
         };
 
-        this.path.public = this.path.root + '/public';
-        this.path.app = this.path.root + '/app';
+        this.path = {
+            root : path.dirname(require.main.filename) + '/'
+        };
 
-        let config = JSON.parse(fs.readFileSync(this.path.app + '/config/config.json', 'utf8'));
+        this.path.public = this.path.root + 'public/';
+        this.path.app = this.path.root + 'app/';
 
-        for(let entry in config) {
-            this[entry] = config[entry];
+        if(fs.existsSync(this.path.app + this.config.file)) {
+            let jsonConfig = JSON.parse(fs.readFileSync(this.path.app + this.config.file, 'utf8'));
+
+            for(let entry in jsonConfig) {
+                this[entry] = jsonConfig[entry];
+            }
         }
 
         console.log(this);
